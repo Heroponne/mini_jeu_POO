326cae5f226f0ac1ea255ab7c3db458aeffa4cac
Heroponne <florence.orgeret@gmail.com>
2019-10-15 21:31:21 +0200

    retouches rubocop

--- app_2.rb
+++ app_2.rb
@@ -6,80 +6,88 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-puts " --------------------------------------------------------------"
+puts ' --------------------------------------------------------------'
 puts "| Bienvenue dans l'arène de 'ILS VEULENT TOUS MA POO' (x_x) !! |"
-puts "|                                                              |"
+puts '|                                                              |'
 puts "|    Le but du jeu est d'être le DERNIER survivant (^o^)//     |"
-puts " --------------------------------------------------------------"
-sleep (3)
+puts ' --------------------------------------------------------------'
+sleep(3)
 puts "Choisis ton nom, guerrier :\n \n"
-print "> "
-name_given = gets.chomp # définition du name du HumanPlayer
+print '> '
+name_given = gets.chomp # definition du name du HumanPlayer
 
 puts "Bienvenue, #{name_given} !! Et bon courage face à tes féroces adversaires...\n \n"
 sleep(2)
 
-user = HumanPlayer.new("#{name_given}") # définition du HumanPlayer
-player1 = Player.new("Josiane La Perfide") # définition du premier adversaire
-player2 = Player.new("José El Conquistador") #définition du deuxième adversaire
+user = HumanPlayer.new(name_given) # definition du HumanPlayer
+player1 = Player.new('Josiane La Perfide') # definition du premier adversaire
+player2 = Player.new('José El Conquistador') # definition du deuxieme adversaire
 
 player1.show_state
 sleep(2)
 player2.show_state
 sleep(2)
-puts "\n"
+puts ' '
 
 enemies_array = [player1, player2]
 
-while user.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0) # le jeu dure tant que l'humain est vivant et qu'au moins un des 2 adversaires l'est aussi
+while user.life_points.positive? && (player1.life_points.positive? || player2.life_points.positive?)
+  # le jeu dure tant que l'humain est vivant et qu'au moins un des 2 adversaires l'est aussi
   user.show_state
   sleep(2)
   puts "FIGHT !!\n \n"
   sleep(2)
   puts "Quelle action veux-tu effectuer ?\n \n"
-  puts "a - chercher une meilleure arme"
+  puts 'a - chercher une meilleure arme'
   puts "s - chercher à se soigner\n \n"
-  puts "attaquer un joueur en vue :"
-  print "0 - "
+  puts 'attaquer un joueur en vue :'
+  print '0 - '
   player1.show_state
-  print "1 -"
+  print '1 - '
   player2.show_state
-  puts "\n"
-  sleep(2)
-  print "> "
+  puts ' '
+  print '> '
   choice = gets.chomp
-  if choice != "a" && choice != "s" && choice != "0" && choice != "1" # si le joueur ne rentre pas une bonne donnée, on lui relaisse une chance
-  	puts "C'est pas un choix, ça. Rééssaie encore une fois, et choisis mieux, SINON...\n \n"
-  	print "> "
-  	choice = gets.chomp
+  if choice != 'a' && choice != 's' && choice != '0' && choice != '1'
+    # si le joueur ne rentre pas une bonne donnee, on lui relaisse une chance
+    puts "C'est pas un choix, ça. Rééssaie encore une fois, et choisis mieux SINON...\n \n"
+    print '> '
+    choice = gets.chomp
   end
-  if choice == "a"
-  	user.search_weapon
-  	sleep(2)
-  elsif choice == "s"
-  	user.search_health_pack
-  	sleep(2)
-  elsif choice == "0"
-  	user.attacks(player1)
-  	sleep(2)
-  elsif choice == "1"
-  	user.attacks(player2)
-  	sleep(2)
-  else # si le joueur n'a pas rentré une bonne donnée une deuxième fois
-	puts "OK, PUNITION. Tu passes ton tour. Tes actes ont des CONSEQUENCES. Médite là-dessus. Et estime-toi heureux que je t'aie pas divisé ta vie par 2, petit effronté."
+  if choice == 'a'
+    user.search_weapon
+    sleep(2)
+  elsif choice == 's'
+    user.search_health_pack
+    sleep(2)
+  elsif choice == '0'
+    user.attacks(player1)
+    sleep(2)
+  elsif choice == '1'
+    user.attacks(player2)
+    sleep(2)
+  else
+    # si le joueur n'a pas rentre une bonne donnee une deuxieme fois
+    puts "OK, PUNITION. Tu passes ton tour. Tes actes ont des CONSEQUENCES. Médite là-dessus. Et estime-toi heureux que je t'aie pas divisé ta vie par 2, petit effronté."
     sleep(3)
   end
-  enemies_array.each do |enemy| # chaque adversaire attaque une fois le joueur
-  	if enemy.life_points > 0
-  	  enemy.attacks(user)
-  	  sleep(2)
-  	end
+  enemies_array.each do |enemy|
+    # chaque adversaire attaque une fois le joueur
+    if enemy.life_points.positive?
+      enemy.attacks(user)
+      sleep(2)
+    end
+    if enemy.life_points <= 0
+      enemy.life_points = 0
+      puts "#{enemy.name} ne fait rien depuis l'Au-Delà ! Quelle feignasse."
+    end
   end
   puts "=========================\n \n"
 end
 
-if user.life_points > 0 # message de fin de partie
-  puts "BRAVO !! Mais à vaincre sans péril, on triomphe sans gloire. Tu sais, ça ? Alors calmos."
+if user.life_points.positive?
+  # message de fin de partie
+  puts 'BRAVO !! Mais à vaincre sans péril, on triomphe sans gloire. Tu sais, ça ? Alors calmos.'
 else
   puts "Je ne pensais même pas que c'était possible de perdre (o_o) ! Ça mérite des félicitations de consolation !"
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,87 +1,89 @@
+# frozen_string_literal: true
+
+# definit la classe Player et ses methodes personnelles
 class Player
-
   attr_accessor :name, :life_points
-
   def initialize(name_to_save)
+    @name = name_to_save
+    @life_points = 10
 
-  	@name = name_to_save
-  	@life_points = 10
+    def self.show_state
+      # affiche l'etat d'un joueur
+      puts "#{@name} a #{@life_points} points de vie"
+    end
 
-  	def self.show_state # affiche l'état d'un joueur
-  	  puts "#{@name} a #{@life_points} points de vie"
-  	end
+    def self.gets_damage(damage_received)
+      # le Player recoit des degats
+      @life_points -= damage_received
+      # determine si le joueur est mort
+      puts "le joueur #{@name} a été tué !" if @life_points <= 0
+    end
 
-  	def self.gets_damage(damage_received) # le Player reçoit des dégâts
-  	  @life_points = @life_points - damage_received
-  	  if @life_points <= 0 # détermine si le joueur est mort
-  	  	puts "le joueur #{@name} a été tué !"
-  	  end
-  	end
-
-  	def self.attacks(other_player) # le Player inflige une certaine quantité de dégâts à un autre Player
+    def self.attacks(other_player)
+      # le Player inflige une certaine quantite de degats a un autre Player
       puts "#{@name} attaque #{other_player.name}"
-      random_damage = compute_damage # nombre de dégâts mis dans une variable
-      puts "il lui inflige #{random_damage} points de dommage" 
-      other_player.gets_damage(random_damage) 
-  	end
+      random_damage = compute_damage # nombre de degats mis dans une variable
+      puts "il lui inflige #{random_damage} points de dommage"
+      other_player.gets_damage(random_damage)
+    end
 
-  	def compute_damage # détermination du nombre de dégâts infligés
-      return rand(1..6)
+    def compute_damage
+      # determination du nombre de degats infliges
+      rand(1..6)
     end
   end
 end
 
+# definit la classe enfant HumanPlayer et ses methodes particulieres
 class HumanPlayer < Player
-
   attr_accessor :weapon_level
-
   def initialize(name_to_save)
-  	super(name_to_save) # invocation de la classe Player
-  	@weapon_level = 1 # ajout de la variable "niveau de l'arme"
-  	@life_points = 100 # modification du life_points pour en attribuer une nouvelle valeur
-    
-    def self.show_state # modification de la méthode show_state dans la classe HumanPlayer
+    super(name_to_save) # invocation de la classe Player
+    @weapon_level = 1 # ajout de la variable "niveau de l'arme"
+    @life_points = 100 # modif du life_points pour attribuer une nouvelle valeur
+    def self.show_state
+      # modification de la methode show_state dans la classe HumanPlayer
       puts "#{@name} a #{@life_points} points de vie et a une arme de niveau #{@weapon_level}"
     end
   end
 
-  def compute_damage # définit un nouveau ratio de dégâts
-    rand(1..6) * @weapon_level    	
+  def compute_damage
+    # definit un nouveau ratio de degats
+    rand(1..6) * @weapon_level
   end
-  
-  def search_weapon 
-  	def new_weapon_level_value
-  	  rand(1..6) 
-  	end
-  	new_weapon_level = new_weapon_level_value # définit le niveau de la nouvelle arme trouvée
+
+  def search_weapon
+    # modifie la valeur de l'arme du joueur
+    def new_weapon_level_value
+      rand(1..6)
+    end
+    # definit le niveau de la nouvelle arme
+    new_weapon_level = new_weapon_level_value
     puts "Tu as trouvé une arme de niveau #{new_weapon_level}"
-    if new_weapon_level > @weapon_level # détermine si on garde l'arme trouvée (son niveau est supérieur à celle possédée)
+    if new_weapon_level > @weapon_level
+      # determine si on garde l'arme trouvee (niveau est superieur a celle possedee)
       @weapon_level = new_weapon_level
-      puts "Wesh, elle est plus badass que ton arme actuelle, vas-y garde là et pimpe ta puissance !!"
-    else # on ne garde pas l'arme trouvée
+      puts 'Wesh, elle est plus badass que ton arme actuelle, vas-y garde là et pimpe ta puissance !!'
+    else # on ne garde pas l'arme trouvee
       puts "Oh. C'est pas ouf, cette arme. Garde pas ça, on ne se contente que du meilleur, quand on est un champion. CHAMPION."
     end
   end
 
   def search_health_pack
-  	def health_pack_value
-  	  rand(1..6)
-  	end
-  	health_pack = health_pack_value # définit la valeur du pack de santé trouvé
-  	if health_pack == 1 # pas de changement
-  	  puts "Vous n'avez rien trouvé, ça sent le roussi :("
-  	elsif health_pack > 1 && health_pack < 6 # la vie augmente de 50 points
-  	  @life_points = @life_points + 50
-  	  if @life_points > 100 # sans dépasser 100
-  	    @life_points = 100
-  	  end
-  	  puts "Ah, ça fait du bien !! :) Tu regagnes 50 points de vie !!"
-  	else # la vie augmente de 80 points
-  	  @life_points = @life_points + 80
-  	  if @life_points > 100 # sans dépasser 100
-  	    @life_points = 100
-  	  end
-  	  puts "OMG petit veinard, t'as trouvé un pack de 80 points de vie !! Ça refait une santé, ça."
-  	end  	
+    def health_pack_value
+      rand(1..6)
+    end
+    health_pack = health_pack_value # definit la valeur du pack de sante trouve
+    if health_pack == 1 # pas de changement
+      puts "Vous n'avez rien trouvé, ça sent le roussi :("
+    elsif health_pack > 1 && health_pack < 6 # la vie augmente de 50 points
+      @life_points += 50
+      @life_points = 100 if @life_points > 100 # sans depasser 100
+      puts 'Ah, ça fait du bien !! :) Tu regagnes 50 points de vie !!'
+    else # la vie augmente de 80 points
+      @life_points += 80
+      @life_points = 100 if @life_points > 100 # sans depasser 100
+      puts "OMG petit veinard, t'as trouvé un pack de 80 points de vie !! Ça refait une santé, ça."
+    end
   end
 end
